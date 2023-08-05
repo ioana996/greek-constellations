729bbfad0805ee9aebfb9a7e345b585f5830acf0
ioanastroinea <stroineaioana@gmail.com>
2023-08-05 17:29:17 +0200

    it works

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppContext, AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+App.getInitialProps = async (appContext: AppContext) => {
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+  return { ...pageProps };
+};
+
+export default App;
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,54 @@
+import { Constellation } from "@/components/Card/types";
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import cassiopeia from "assets/cassiopeia.jpg";
 
-type Data = {
-  name: string
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Constellation[]>
+// ) {
+//   res.status(200).json([
+//     {
+//       constellation: "Cassiopeia",
+//       image: cassiopeia,
+//       short:
+//         "Cassiopeia is named after the queen of a country on the northern coast of Africa, Aethiopia (not modern Ethiopia). She boasted that she and her daughter Andromeda were more beautiful than the Nereids, the 50 sea nymph attendants of Thetis, the sea goddess, and Poseidon, the sea god.",
+//       readTime: "20 MIN READ",
+//     },
+//     {
+//       constellation: "Taurus",
+//       image: cassiopeia,
+//       short:
+//         "Cassiopeia is named after the queen of a country on the northern coast of Africa, Aethiopia (not modern Ethiopia). She boasted that she and her daughter Andromeda were more beautiful than the Nereids, the 50 sea nymph attendants of Thetis, the sea goddess, and Poseidon, the sea god.",
+//       readTime: "20 MIN READ",
+//     },
+//     {
+//       constellation: "Aries",
+//       image: cassiopeia,
+//       short:
+//         "Cassiopeia is named after the queen of a country on the northern coast of Africa, Aethiopia (not modern Ethiopia). She boasted that she and her daughter Andromeda were more beautiful than the Nereids, the 50 sea nymph attendants of Thetis, the sea goddess, and Poseidon, the sea god.",
+//       readTime: "20 MIN READ",
+//     },
+//     {
+//       constellation: "Libra",
+//       image: cassiopeia,
+//       short:
+//         "Cassiopeia is named after the queen of a country on the northern coast of Africa, Aethiopia (not modern Ethiopia). She boasted that she and her daughter Andromeda were more beautiful than the Nereids, the 50 sea nymph attendants of Thetis, the sea goddess, and Poseidon, the sea god.",
+//       readTime: "20 MIN READ",
+//     },
+//     {
+//       constellation: "Gemini",
+//       image: cassiopeia,
+//       short:
+//         "Cassiopeia is named after the queen of a country on the northern coast of Africa, Aethiopia (not modern Ethiopia). She boasted that she and her daughter Andromeda were more beautiful than the Nereids, the 50 sea nymph attendants of Thetis, the sea goddess, and Poseidon, the sea god.",
+//       readTime: "20 MIN READ",
+//     },
+//     {
+//       constellation: "Leo",
+//       image: cassiopeia,
+//       short:
+//         "Cassiopeia is named after the queen of a country on the northern coast of Africa, Aethiopia (not modern Ethiopia). She boasted that she and her daughter Andromeda were more beautiful than the Nereids, the 50 sea nymph attendants of Thetis, the sea goddess, and Poseidon, the sea god.",
+//       readTime: "20 MIN READ",
+//     },
+//   ]);
+// }
